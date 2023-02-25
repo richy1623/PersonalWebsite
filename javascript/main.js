1aa3cdb4715b30c26fa52170b9e142e2b50dfdee
Richard Paterson <richy1623@gmail.com>
2023-02-25 23:57:21 +0200

    adding support for touch devices

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,33 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
+
 
 function letterEffect(event){
+    console.log(nameDiv.childNodes)
     var i = 0;
 
-    clearInterval(interval);
-
-    interval = setInterval(() => {
-        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-            if (index<i) return event.target.dataset.original[index];
-            return String.fromCharCode(Math.floor(Math.random()*44+47));
-        }).join("");
-        if(i>event.target.dataset.original.length){
-            clearInterval(interval);
-        }
-        i+=1/2;
-    }, 55);
+    nameDiv.childNodes.forEach(child => {
+        if(child instanceof Text) return;
+        let interval = null;
+        clearInterval(interval);
+    
+        interval = setInterval(() => {
+            child.innerText = child.innerText.split("").map((letter, index) => {
+                // if (event.target.dataset.original.charCodeAt(index)==13) return "\n";
+                if (index<i) return child.dataset.original[index];
+                return String.fromCharCode(Math.floor(Math.random()*44+47));
+            }).join("");
+            if(i>child.dataset.original.length){
+                clearInterval(interval);
+            }
+            i+=1/5;
+        }, 80);
+        
+    });
 }
 
 document.getElementById("name").onmouseover = event => letterEffect(event);
-document.getElementsByClassName("body").ontouchstart = event => letterEffect(event);
+let nameDiv = document.getElementById("name");
+document.querySelector("body").addEventListener("touchstart", event => letterEffect(event));
+
+console.log(document.getElementById("name"))
